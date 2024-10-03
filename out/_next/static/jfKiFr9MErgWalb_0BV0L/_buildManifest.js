de725fb5cc2b31160e66846cb6bda627872f9337
imranshakil <imranshakil77@gmail.com>
2024-10-03 22:07:36 +0600

    portfolio update

---
+++ out/_next/static/jfKiFr9MErgWalb_0BV0L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/300-6fad519c93d3780c.js","static/chunks/pages/index-a2ec181cf6a2978e.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
